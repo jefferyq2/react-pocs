1709bd9c0c83bd8dc89325d4f15a74fb1f74e5f8
mitrais <mitrais@mtmac15.mitrais.com>
2016-04-21 13:56:20 +0700

    ios_MediaSocialSignIn

---
+++ ios_MediaSocialSignIn/login.js
@@ -0,0 +1,80 @@
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  NavigatorIOS,
+  TouchableHighlight,
+  ActivityIndicatorIOS
+} from 'react-native';
+
+const simpleAuthClient = require('./lib/simpleauthclient');
+const secrets = require('./secrets.example')
+const profile = require('./profile').default;
+
+class Login extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false
+    };
+  }
+
+  componentWillMount() {
+    simpleAuthClient.configure(secrets);
+  }
+
+  render() {
+    return (
+      <View style={styles.content}>
+        {
+          this.state.loading ? null : this.props.authProviders.map(provider => {
+            return (
+              <TouchableHighlight
+                style={[styles.button, styles[provider]]}
+                onPress={this.onBtnPressed.bind(this, provider)}>
+                <Text style={[styles.buttonText]}>{provider.split('-')[0]}</Text>
+              </TouchableHighlight>
+            );
+          })
+        }
+        <ActivityIndicatorIOS
+            animating={this.state.loading}
+            style={[styles.loading]}
+            size='large' />
+      </View>
+    );
+  }
+
+  onBtnPressed(provider) {
+    this.setState({
+      loading: true
+    });
+    simpleAuthClient.authorize(provider)
+      .then(info => {
+        this.props.navigator.push({
+          title: provider,
+          component: Profile,
+          passProps: {
+            info: info,
+            provider: provider
+          }
+        });
+        this.setState({
+          loading: false
+        });
+      })
+      .catch(error => {
+          React.AlertIOS.alert(
+              'Authorize Error',
+              error && error.description || '');
+        this.setState({
+          loading: false
+        });
+      });
+  }
+
+}
---
+++ ios_MediaSocialSignIn/profile.js
@@ -0,0 +1,64 @@
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  NavigatorIOS,
+  TouchableHighlight,
+  ActivityIndicatorIOS
+} from 'react-native';
+
+class Profile extends Component {
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            name: this.getName(props.provider),
+            picture: this.getPictureLink(props.provider)
+        };
+    }
+    
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image style={styles.pic} source={{uri: this.state.picture}} />
+        <Text style={styles.header}>{this.state.name} </Text>
+        <View style={styles.scroll}>
+            <Text style={styles.mono}>
+            {JSON.stringify(this.props.info, null,4)}
+            </Text>
+        </View>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+  
+  getName(provider){
+      switch(provider){
+          case 'instagram':
+            return this.props.name;
+          default:
+          return this.props.name
+      }
+  }
+  
+  getPictureLink(provider){
+      switch (provider){
+          case 'google-web':
+            return this.props.info.picture;
+          case 'instagram':
+          return this.props.info.data.profile_picture;
+      }
+  }
+}
