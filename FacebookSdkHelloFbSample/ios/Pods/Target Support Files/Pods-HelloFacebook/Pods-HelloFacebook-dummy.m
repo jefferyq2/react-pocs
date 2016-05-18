b92846231d7c0bc330843d08ca5fd40cbe9c277b
WendySanarwanto <wendy.sanarwanto@gmail.com>
2016-05-18 08:54:57 +0800

    Initial Commit

---
+++ FacebookSdkHelloFbSample/ios/Pods/Target Support Files/Pods-HelloFacebook/Pods-HelloFacebook-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HelloFacebook : NSObject
+@end
+@implementation PodsDummy_Pods_HelloFacebook
+@end
---
+++ FacebookSdkHelloFbSample/ios/Pods/Target Support Files/Pods-HelloFacebookTests/Pods-HelloFacebookTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HelloFacebookTests : NSObject
+@end
+@implementation PodsDummy_Pods_HelloFacebookTests
+@end
