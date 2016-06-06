431318b2a83bd0ddccf0b2bc56a333943ac58131
Rai_A <raisuardhyana.arijasa@mitrais.com>
2016-06-06 16:20:52 +0800

    add file

--- SampleProjectStructure/apps/modules/home/screen_home.js
+++ SampleProjectStructure/apps/modules/home/screen_home.js
@@ -5,25 +5,52 @@ import React, {
     Component,
     StyleSheet,
     Text,
-    View
+    View,
+    DrawerLayoutAndroid,
+    TouchableHighlight
 } from 'react-native';
 
 import Styles from './style_home';
 
 class HomeScreen extends Component {
+    openDrawer() {
+    this.refs['DRAWER'].openDrawer()
+  };
     render() {
-        return (
-            <View style={Styles.container}>
-                <Text style={Styles.welcome}>
-                    Welcome to React Native!
-                </Text>
-                <Text style={Styles.instructions}>
-                    To get started, edit index.android.js
-                </Text>
-                <Text style={Styles.instructions}>
-                    Shake or press menu button for dev menu
+        
+        var navigationView = (
+            <View style={{flex: 1, backgroundColor: '#fff'}}>
+                <Text style={{margin: 10, fontSize: 15, textAlign: 'left'}}>
+                    I'm in the Drawer!
                 </Text>
             </View>
+        );
+    
+        return (
+            <DrawerLayoutAndroid
+              drawerWidth={300}
+              ref={'DRAWER'}
+              drawerPosition={DrawerLayoutAndroid.positions.Left}
+              renderNavigationView={() => navigationView}>
+            <View style={{flex: 1, alignItems: 'center'}}>
+              <Text style={{margin: 10, fontSize: 15, textAlign: 'right'}}>Hello</Text>
+              <Text style={{margin: 10, fontSize: 15, textAlign: 'right'}}>World!</Text>
+              <TouchableHighlight onPress={this.openDrawer}>
+                <Text>{'Open Drawer'}</Text>
+              </TouchableHighlight>
+            </View>
+          </DrawerLayoutAndroid>
+            // <View style={Styles.container}>
+            //     <Text style={Styles.welcome}>
+            //         Welcome to React Native!
+            //     </Text>
+            //     <Text style={Styles.instructions}>
+            //         To get started, edit index.android.js
+            //     </Text>
+            //     <Text style={Styles.instructions}>
+            //         Shake or press menu button for dev menu
+            //     </Text>
+            // </View>
         );
     }
 }
